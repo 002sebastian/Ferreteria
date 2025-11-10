cd37265247962b3879048ada577492bc3d06d3bf
002sebastian <sebastian.dazam@campusucc.edu.co>
2025-11-10 01:05:05 -0500

    sdfasd

--- view/footer.php
+++
@@ -1,7 +0,0 @@
-        </main>
-        <footer style="margin-top: 20px; padding: 20px; text-align: center; color: #666;">
-            <p>&copy; <?php echo date('Y'); ?> Ferretería MVC</p>
-        </footer>
-    </div>
-</body>
-</html>
